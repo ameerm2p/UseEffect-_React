ea5f1499ad92a2a1502afd4955c323826f14bc2d
ameerm2p <ameer.basha@m2pfintech.com>
2023-11-29 16:39:43 +0530

    UseEffect-_usaState_onChange_onSubmit_React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  useEffect(() => console.log("One time useEffect"), []);
+  useEffect(() => console.log("empty dependency useEffect"));
+  useEffect(() => console.log(count), [count]);
+
+  const [username, setUsername] = useState("");
+  const handler = (e) => {
+    setUsername(e.target.value);
+  };
+
+  //const [email,setEmail]=useState("");
+  //const[passwors, setPassword]=useState("");
+
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+  const onchangehandler = (e) => {
+    setData(({
+      ...data,
+     // [e.target.name]: [e.target.value]  array
+     [e.target.name]: e.target.value
+    }));
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(data);
+    console.log(data.email);
+    console.log(data.password);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>you clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <br></br>
+      <div>
+        <center>
+          <input
+            type="text"
+            placeholder="username"
+            value={username}
+            name="username"
+            onChange={handler}
+          ></input>
+        </center>
+        <p>Username: {username}</p>
+      </div>
+      <br></br>
+      <div>
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            placeholder="email"
+            value={data.email}
+            name="email"
+            onChange={onchangehandler}
+          ></input> <br></br> <br></br>
+          <input
+            type="text"
+            placeholder="password"
+            value={data.password}
+            name="password"
+            onChange={onchangehandler}
+          ></input> <br></br> <br></br>
+          <input type="submit"></input>
+        </form>
+      </div>
     </div>
   );
 }
-
 export default App;
